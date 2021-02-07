ecdfd7ba462b5927f28fc6666a59a0e519e8c90e
N E W E <59024021+NEWE69@users.noreply.github.com>
2021-02-07 22:21:00 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+from colorama import init, Fore
+import string
+import ctypes
+
+init(convert=True)
+ctypes.windll.kernel32.SetConsoleTitleW("Amazon Gift Card Generator By NEWE")
+text = '''
+ ▄▄▄       ███▄ ▄███▓ ▄▄▄      ▒███████▒ ▒█████   ███▄    █   ▄████ ▓█████  ███▄    █ 
+▒████▄    ▓██▒▀█▀ ██▒▒████▄    ▒ ▒ ▒ ▄▀░▒██▒  ██▒ ██ ▀█   █  ██▒ ▀█▒▓█   ▀  ██ ▀█   █ 
+▒██  ▀█▄  ▓██    ▓██░▒██  ▀█▄  ░ ▒ ▄▀▒░ ▒██░  ██▒▓██  ▀█ ██▒▒██░▄▄▄░▒███   ▓██  ▀█ ██▒
+░██▄▄▄▄██ ▒██    ▒██ ░██▄▄▄▄██   ▄▀▒   ░▒██   ██░▓██▒  ▐▌██▒░▓█  ██▓▒▓█  ▄ ▓██▒  ▐▌██▒
+ ▓█   ▓██▒▒██▒   ░██▒ ▓█   ▓██▒▒███████▒░ ████▓▒░▒██░   ▓██░░▒▓███▀▒░▒████▒▒██░   ▓██░
+ ▒▒   ▓▒█░░ ▒░   ░  ░ ▒▒   ▓▒█░░▒▒ ▓░▒░▒░ ▒░▒░▒░ ░ ▒░   ▒ ▒  ░▒   ▒ ░░ ▒░ ░░ ▒░   ▒ ▒ 
+  ▒   ▒▒ ░░  ░      ░  ▒   ▒▒ ░░░▒ ▒ ░ ▒  ░ ▒ ▒░ ░ ░░   ░ ▒░  ░   ░  ░ ░  ░░ ░░   ░ ▒░
+  ░   ▒   ░      ░     ░   ▒   ░ ░ ░ ░ ░░ ░ ░ ▒     ░   ░ ░ ░ ░   ░    ░      ░   ░ ░ 
+      ░  ░       ░         ░  ░  ░ ░        ░ ░           ░       ░    ░  ░         ░ 
+                               ░                                                      
+'''
+
+def gen(fix, amount):
+    while fix <= amount:
+        code = ('').join(random.choices(string.ascii_letters.upper() + string.digits.upper(), k=13))
+        f.write(code.upper() + '\n')
+        print(Fore.GREEN + code.upper())
+        fix += 1
+        ctypes.windll.kernel32.SetConsoleTitleW("Generated Codes: " + str(fix) + "/" + str(amount))
+
+print(Fore.RED + text + Fore.WHITE)
+f = open('amazon_codes.txt', 'a')
+print()
+print(Fore.RED + 'Enter amount of codes to generate: ')
+amount = int(input())
+gen(1, amount)
